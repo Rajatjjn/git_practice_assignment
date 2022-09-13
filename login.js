5990dbe235111475dc59666b4102d76e991efe4d
rajatjjn <rajatjjn0510@gmail.com>
2022-09-13 17:35:05 +0530

    prime no assignment

---
+++ login.js
@@ -0,0 +1,13 @@
+for(i=1;i<=13;i++){
+    count=0;
+    for(j=1;j<=i;j++){
+        if(i%j===0){
+            
+        }
+    }
+    }if(count==2){
+        console.log("Yes");
+        
+    }else{
+        console.log(No);
+    }
